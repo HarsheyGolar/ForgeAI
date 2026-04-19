fe97998a8437810ea788d830c24fafc5becdbc9a
HarsheyGolar <harsheygolar@gmail.com>
2026-04-19 12:17:58 +0530

    Fix env variable names

--- auth.py
+++ auth.py
@@ -12,8 +12,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-supabase_url = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
-supabase_api_key = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_api_key = os.getenv("SUPABASE_KEY")
 
 supabase : Client = create_client(supabase_url,supabase_api_key)
 
